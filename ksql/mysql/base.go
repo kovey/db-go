e2ceaae72dcd5d53b8623550c6f23b19db8173fc
kovey <kovey@vip.qq.com>
2024-11-13 13:55:04 +0800

    bugs fixed

--- ksql/mysql/base.go
+++ ksql/mysql/base.go
@@ -1,13 +1,13 @@
 package mysql
 
-import "github.com/kovey/db-go/v3"
+import ksql "github.com/kovey/db-go/v3"
 
 type base struct {
 	conn  ksql.ConnectionInterface
 	empty bool
 }
 
-func (b *base) SetConn(conn ksql.ConnectionInterface) {
+func (b *base) WithConn(conn ksql.ConnectionInterface) {
 	b.conn = conn
 }
 
